c83628a54c21bf0835e429d4f197b508b6931fdf
Luis Olivárez <luiznaiper@Luiss-MacBook-Pro.local>
2022-04-29 17:12:37 -0300

    added css for tablet and try / catch error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&?api_key=a87e0d4e-0554-44c3-88c6-d0fc4983e2fa'
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=a87e0d4e-0554-44c3-88c6-d0fc4983e2fa'
 
-    async function reloadImg(){
-        const res = await fetch(API_URL)
-        const data = await res.json()
-        const img1 = document.getElementById('img1')
-        const img2= document.getElementById('img2')
-        const img3 = document.getElementById('img3')
-        console.log(data)
-        img1.src = data[0].url
-        img2.src = data[1].url
-        img3.src = data[2].url
+
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=3&api_key=a87e0d4e-0554-44c3-88c6-d0fc4983e2fa'
+
+    const reloadImg = async () => {
+        try{
+            const res = await fetch(API_URL_RANDOM)
+            const status = res.status
+            if (status !== 200) throw new Error(`New error in ${status}`)
+            const data = await res.json()
+            console.log(data)
+            const img1 = document.getElementById('img1')
+            const img2= document.getElementById('img2')
+            const img3 = document.getElementById('img3')
+            img1.src = data[0].url
+            img2.src = data[1].url
+            img3.src = data[2].url
+        } catch (error){
+            console.log(error)
+            const errorNode = document.querySelector('#random-error')
+            errorNode.innerText = ` ${error}`
+        }
+        
 }
-  
-reloadImg()
 
+const loadFavImg = async () => {
+    try{
+        const res = await fetch(API_URL_FAVORITES)
+        const status = res.status
+        if (status !== 200) throw new Error(`New error in ${status}`)
+        const data = await res.json()
+        console.log(data)
+        } catch (error){
+            console.log(error)
+            const errorNode = document.querySelector('#random-error')
+            errorNode.innerText = `${error}`
+        }
+        
+    }
 
+    //async function saveFavorites
+
+    reloadImg();
+    loadFavImg()
