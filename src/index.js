a5fe91b5c8f885457abe62f56064d83160d499be
Luis Olivárez <luiznaiper@Luiss-MacBook-Pro.local>
2022-05-02 06:16:16 -0300

    added save favorites functions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=a87e0d4e-0554-44c3-88c6-d0fc4983e2fa'
 
 
-const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=3&api_key=a87e0d4e-0554-44c3-88c6-d0fc4983e2fa'
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?&api_key=a87e0d4e-0554-44c3-88c6-d0fc4983e2fa'
 
     const reloadImg = async () => {
         try{
@@ -10,6 +10,7 @@ const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=3&api_k
             const status = res.status
             if (status !== 200) throw new Error(`New error in ${status}`)
             const data = await res.json()
+            console.log('Load')
             console.log(data)
             const img1 = document.getElementById('img1')
             const img2= document.getElementById('img2')
@@ -31,6 +32,7 @@ const loadFavImg = async () => {
         const status = res.status
         if (status !== 200) throw new Error(`New error in ${status}`)
         const data = await res.json()
+        console.log('Favorites')
         console.log(data)
         } catch (error){
             console.log(error)
@@ -40,7 +42,30 @@ const loadFavImg = async () => {
         
     }
 
-    //async function saveFavorites
+    const saveFavorites = async () =>{
+        try{
+            const res = await fetch(API_URL_FAVORITES, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    image_id: 'dje'
+                })
+            })
+            const status = res.status
+            if (status !== 200) throw new Error(`New error in ${status}`)
+            const data = await res.json()
+            console.log('Save')
+            console.log(res)
+        } catch (error){
+            console.log(error.message)
+            const errorNode = document.querySelector('#random-error')
+            errorNode.innerText = `${error.message}`
+        }
+       
+    }
 
     reloadImg();
     loadFavImg()
+    saveFavorites()
